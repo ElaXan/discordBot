97034e060caefab127d2a6330f8652acc4a69a93
ElaXan <zero@elaxan.com>
2022-11-30 12:28:09 +0000

    Add play music bot :)

--- index.js
+++ index.js
@@ -60,6 +60,7 @@ client.on(Events.InteractionCreate, async interaction => {
     if (!code) return;
     try {
         await code.execute(interaction);
+        interaction.member.voice.channel.join();
     } catch (error) {
         console.error(error);
         await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
