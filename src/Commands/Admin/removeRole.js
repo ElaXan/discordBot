f90059a9794dbfa05ba27af8aeeeecc04568262e
ElaXan <zero@elaxan.com>
2022-12-08 04:46:06 +0000

    Add command "removerole" and  fix error

--- src/Commands/Admin/removeRole.js
+++ src/Commands/Admin/removeRole.js
@@ -1,17 +1,17 @@
 module.exports = {
     data: {
-        name: "addrole",
-        description: "Add a role to a user",
+        name: "removerole",
+        description: "Remove a role from a user",
         options: [
             {
                 name: "user",
-                description: "The user to add the role to",
+                description: "The user to remove the role from",
                 type: 6,
                 required: true
             },
             {
                 name: "role",
-                description: "The role to add to the user",
+                description: "The role to remove from the user",
                 type: 8,
                 required: true
             }
@@ -23,9 +23,10 @@ module.exports = {
         const user = interaction.options.getUser("user");
         const role = interaction.options.getRole("role");
         const member = interaction.guild.members.cache.get(user.id);
-        const { log } = require("../../log/log");
-        member.roles.add(role);
-        await interaction.reply({ content: `Added role ${role.name} to ${user.tag}`});
-        log("AddRole", `Added role ${role.name} to ${user.tag}`, interaction.user.tag, interaction.user.id, interaction.channel.id, interaction.guild.id);
+        const log = require("../../log/log").log
+        member.roles.remove(role);
+        log("RemoveRole", `Removed role ${role.name} from ${user.tag}`, interaction.user.tag, interaction.user.id, interaction.channel.id, interaction.guild.id);
+        return await interaction.reply({ content: `Removed role ${role.name} from ${user.tag}`});
+        
     }
 }
