e296a6ac210b2680554ab6528e40d120ab0be247
ElaXan <zero@elaxan.com>
2022-12-16 01:18:58 +0000

    Fix

--- src/Commands/Genshin/gm.js
+++ src/Commands/Genshin/gm.js
@@ -95,7 +95,7 @@ searchGM = async (search, categoryId) => {
         },
         async autocomplete(interaction) {
             const focusedValue = interaction.options.getFocused();
-            const search = focusedValue.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase()).replace(/( Or |Or | Or| The |The | The| A |A | A|Of | Of| Of )/g, letter => letter.toLowerCase());
+            const search = focusedValue.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase()).replace(/( Or | The | A | Of )/g, letter => letter.toLowerCase());
             //const search = focusedValue.replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())));
             const choices = [];
             const fs = require('fs');
@@ -115,7 +115,6 @@ searchGM = async (search, categoryId) => {
                     if (line.startsWith("//")) {
                         category = line.replace("//", "").replace(" ", "");
                     }
-                    console.log(search)
                     if (line.includes(search)) {
                         if (line.length < 1) {
                             choices.push({
@@ -137,7 +136,7 @@ searchGM = async (search, categoryId) => {
             const search = interaction.options.getString('search');
             const category = interaction.options.getString('category');
             const searchUpperCase = search.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
-            const searchLowerCase = search.replace(/( Or |Or | Or| The |The | The| A |A | A|Of | Of| Of )/g, letter => letter.toLowerCase());
+            const searchLowerCase = search.replace(/( Or | The | A | Of )/g, letter => letter.toLowerCase());
             const searchResult = await searchGM(searchLowerCase, category);
             if (searchResult.id === "Not Found" && searchResult.name === "Not Found" && searchResult.category === "Not Found") {
                 const embed = new EmbedBuilder()
@@ -150,7 +149,7 @@ searchGM = async (search, categoryId) => {
                         iconURL: interaction.user.displayAvatarURL()
                     });
                 await interaction.reply({ embeds: [embed] });
-                log.log("GM", `Not found ID for ${searchUpperCase}}`, interaction.user.tag, interaction.user.id, interaction.channel.id, interaction.guild.id)
+                log.log("GM", `Not found ID for ${searchUpperCase}`, interaction.user.tag, interaction.user.id, interaction.channel.id, interaction.guild.id)
             } else {
                 const embed = new EmbedBuilder()
                     .setTitle('Search Result')
