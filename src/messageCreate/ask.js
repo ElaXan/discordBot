4d8b3ee4d4e12a2ef9186f511e3b9c4633fd65a3
ElaXan <zero@elaxan.com>
2022-12-29 12:17:11 +0000

    Add OpenAI API for asking, etc (Testing)

---
+++ src/messageCreate/ask.js
@@ -0,0 +1,25 @@
+const { Configuration, OpenAIApi } = require('openai');
+const { OPENAI_API_KEY, Prefix, OWNER_ID } = require("../../config.json")
+
+module.exports = {
+    name: 'ask',
+    async execute(message) {
+        if (!message.content.startsWith(Prefix)) return;
+        if (!message.content.startsWith(`${Prefix}${this.name}`)) return;
+        if (message.author.id !== OWNER_ID) return message.reply("Only bot can ask questions.")
+        const prompt = message.content.slice(this.name.length + 2);
+        const config = new Configuration({
+            apiKey: OPENAI_API_KEY
+        });
+        const openai = new OpenAIApi(config);
+        const completion = await openai.createCompletion(
+            {
+              model: "text-davinci-003",
+              prompt: prompt,
+              temperature: 0.9,
+              max_tokens: 100,
+            },
+          );
+        message.reply(completion.data.choices[0].text)
+    }
+}
